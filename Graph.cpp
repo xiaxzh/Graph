2faa8f4bc1d93bb10f666415e71de96fcf6ef7d3
psy054duck <wangchlin0@163.com>
2016-12-04 21:12:06 +0800

    update the information of nodes

--- Graph.cpp
+++ Graph.cpp
@@ -15,14 +15,23 @@ Graph::Graph(const string& filename) {
         _adjacency[origin][target] = distance;
         _adjacency[target][origin] = distance;
     }
-    _spot.push_back(Node("cateen", "中国广东省广州市番禺区外环东路132号中山大学广州东校区", "123"));
-    _spot.push_back(Node("gogo", "中国广东省广州市番禺区大学城中二横路1号", "123"));
-    _spot.push_back(Node("public", "中国广东省广州市番禺区外环东路132号中山大学广州东校区", "123"));
-    _spot.push_back(Node("SDCS", "中国广东省广州市番禺区外环东路132号中山大学广州东校区", "123"));
-    _spot.push_back(Node("north", "中国广东省广州市番禺区外环东路132号中山大学广州东校区", "123"));
-    _spot.push_back(Node("forest", "中国广东省广州市番禺区外环东路132号中山大学广州东校区", "123"));
-    _spot.push_back(Node("administration", "中国广东省广州市番禺区外环东路132号中山大学广州东校区", "123"));
-    _spot.push_back(Node("library", "中国广东省广州市番禺区外环东路132号中山大学广州东校区", "123"));
-    _spot.push_back(Node("gym", "中国广东省广州市番禺区外环东路132号中山大学广州东校区", "123"));
-    _spot.push_back(Node("dormitory", "中国广东省广州市番禺区外环东路132号中山大学广州东校区", "123"));
+    _spot.push_back(Node("cateen", "中国广东省广州市番禺区外环东路132号中山大学广州东校区", "None"));
+    _spot.push_back(Node("gogo", "中国广东省广州市番禺区大学城中二横路1号", "+86 20-39330333"));
+    _spot.push_back(Node("public", "中国广东省广州市番禺区外环东路132号中山大学东校区", "None"));
+    _spot.push_back(Node("SDCS", "中国广东省广州市番禺区外环东路132号中山大学广州东校区", "None"));
+    _spot.push_back(Node("north", "中国广东省广州市番禺区外环东路132号中山大学东校区", "None"));
+    _spot.push_back(Node("forest", "中国广东省广州市番禺区大学城谷河南路与逸仙大道交叉口西南100米", "None"));
+    _spot.push_back(Node("administration", "中国广东省广州市番禺区外环东路132号中山大学东校区", "None"));
+    _spot.push_back(Node("library", "中国广东省广州市番禺区新造镇广州大学城外环东路132号", "+86 20-39332622"));
+    _spot.push_back(Node("gym", "中国广东省广州市番禺区外环东路132号中山大学广州东校区", "None"));
+    _spot.push_back(Node("dormitory", "中国广东省广州市番禺区贝岗村大街与青蓝街交叉口东200米", "None"));
+}
+
+int Graph::getIndex(const string& spotName) {
+    for (int i = 0; i < count; ++i) {
+        if (_spot[i]._spotName == spotName) {
+            return i;
+        }
+    }
+    return -1;
 }
